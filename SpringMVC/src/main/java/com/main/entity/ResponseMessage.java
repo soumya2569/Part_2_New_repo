aa19d9ae8d1a85882e08ad5f44febd1e42bcacca
Shanthi-Swaroop-P <62928269+Shanthi-Swaroop-P@users.noreply.github.com>
2020-06-08 17:59:34 +0530

    Second Commit

---
+++ SpringMVC/src/main/java/com/main/entity/ResponseMessage.java
@@ -0,0 +1,22 @@
+package com.main.entity;
+
+public class ResponseMessage {
+	
+	private String messageCode;
+	private String messageDesc;
+	
+	public String getMessageCode() {
+		return messageCode;
+	}
+	public void setMessageCode(String messageCode) {
+		this.messageCode = messageCode;
+	}
+	public String getMessageDesc() {
+		return messageDesc;
+	}
+	public void setMessageDesc(String messageDesc) {
+		this.messageDesc = messageDesc;
+	}
+	
+	
+}
